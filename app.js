b875fc10e22bbda3c9e8f55f13d1f500a1b7a41f
HollysMason <ibondar204@gmail.com>
2016-11-02 16:53:49 +0300

    tying do it with angular

--- app.js
+++ app.js
@@ -1,43 +1,119 @@
-function showError (container) {
-  var divContainer = container.parentNode.childNodes[1];
-  divContainer.classList.remove("correct_icon");
-  divContainer.classList.add("error_icon");
-  container.className = "error"
+//
+// function showError (container) {
+//   var divContainer = container.parentNode.childNodes[1];
+//   divContainer.classList.remove("correct_icon");
+//   divContainer.classList.add("error_icon");
+//   container.className = "error"
+//
+//   return false;
+// }
+//
+// function showCorrect (container) {
+//   container.parentNode.childNodes[1].classList.add("correct_icon");
+//   container.className = "correct";
+//
+//   return true;
+// }
+//
+//
+//
+// function check() {
+//   var form = document.forms.signUp;
+//   var firstInput =  form.elements.name;
+//
+//   var a = !firstInput.value ? showError(firstInput) : showCorrect(firstInput);
+//   console.log(a);
+// }
+//
+// $(document).ready(function(){
+//
+//   $.ajax({
+// method: "POST",
+// url: "http://codeit.pro/frontTestTask/user/registration",
+// data: {
+// name: 'dasd',
+// secondname: 'dsda',
+// email: 'sadasd@dasda.da',
+// gender: 'male',
+// pass: "asdasdasd",
+// checkbox: true
+// }
+// })
+// .done(function( msg ) {
+// console.log(msg);
+// });
+//
+// });
+(function () {
+  'use strict';
 
-  return false;
-}
+  angular.module('FormApp', [])
+  .controller('formController', formController)
+  .service('formService', formService);
 
-function showCorrect (container) {
-  container.parentNode.childNodes[1].classList.add("correct_icon");
-  container.className = "correct";
+  formController.$inject = ['formService'];
+  function formController (formService) {
+    var reg = this;
 
-  return true;
-}
+    reg.complited = false;
+    reg.errorCondition = "";
 
-function check() {
-  var form = document.forms.signUp;
-  var firstInput =  form.elements.name;
+    reg.logSMT = function () {
+      var promise = formService.registraiteUser();
 
-  var a = !firstInput.value ? showError(firstInput) : showCorrect(firstInput);
-  console.log(a);
-}
+      promise.then(function (response) {
+        console.log(response.data);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
 
-$(document).ready(function(){
+    };
 
-  $.ajax({
-method: "POST",
-url: "http://codeit.pro/frontTestTask/user/registration",
-data: {
-name: 'dasd',
-secondname: 'dsda',
-email: 'sadasd@dasda.da',
-gender: 'male',
-pass: "asdasdasd",
-checkbox: true
-}
-})
-.done(function( msg ) {
-console.log(msg);
-});
+    // reg.submit = function () {
+    //   var count = 1;
+    //     for (item in reg.user) {
+    //       count++;
+    //     }
+    //     if (count === 7) {
+    //       var promise = formService.registraiteUser();
+    //
+    //       promise.then(function (response) {
+    //         console.log(response.data);
+    //       })
+    //       .catch(function (error) {
+    //         console.log(error);
+    //       });
+    //
+    //     } else if (count < 7 ) {
+    //       reg.errorCondition = "Please fill the inputs."
+    //     } else {
+    //       reg.errorCondition = "Please accept condition."
+    //     }
+    //
+    // };
 
-});
+  }
+
+  formService.$inject = ['$http'];
+  function formService ($http) {
+      var service = this;
+
+    service.registraiteUser = function () {
+      var response = $http({
+        method: "GET",
+        url: ("http://codeit.pro/frontTestTask/user/registration"),
+        params: {
+          name: 'dasd',
+          secondname: 'dsda',
+          email: 'sadasd@daasda.da',
+          gender: 'male',
+          pass: "123457846",
+          checkbox: true
+        }
+      });
+
+        return response;
+    };
+  }
+})()
